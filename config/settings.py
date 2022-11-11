e9bd4092f9eca9ccb30a79220c6cf0f59032ef29
alevcoder <alevcoder1@gmail.com>
2022-11-11 17:16:42 +0500

    changes

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ SECRET_KEY = 'django-insecure-$9bmpy61slw(t(ghmmwuqcq_$bg58jwm+e3687_9!ka_+*ew+2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -105,27 +107,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LOCALE_PATHS = (
-    os.path.join(BASE_DIR, 'locale'),
-)
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = True
-
 USE_TZ = True
 
+USE_I18N = True
+USE_L10N = True
 
-gettext = lambda s: s
-LANGUAGES = (
-    ('uz',  gettext('Uzbek')),
-    ('ru',  gettext('Russian')),
-    ('en',  gettext('English')),
-    
-)
-MODELTRANSLATION_DEFAULT_LANGUAGE = 'uz'
+LANGUAGE = [
+    ('en', _('English')),
+    ('ru', _('Rusia')),
+    ('tr', _('Turkish')),
+    ('uz', _('O`zbek')),
+]
+LOCALE_PATH = (os.path.join(BASE_DIR, 'locale'))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- config/urls.py
+++ config/urls.py
@@ -17,20 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-# from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('products/', include("product.urls")),
     path('order/', include("operation.urls")),
-    path('i18n/', include('django.conf.urls.i18n')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += i18n_patterns(
-#     # path('pages/', include('django.contrib.flatpages.urls')),
-#     # path('contact/', include("contact.urls")),
-#     path("", include("product.urls")),
-#     prefix_default_language=False
-#    ) 
