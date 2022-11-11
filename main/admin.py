9125a08bb7c519456e43b9cae92e526f4034db37
alevcoder <alevcoder1@gmail.com>
2022-11-11 22:37:57 +0500

    bir koinchi siz ham

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-
+from modeltranslation.admin import TranslationAdmin
 from main.models import Contact, Subscription, Setting, Blog
 
 # Register your models here.
-admin.site.register(Contact)
-admin.site.register(Blog)
 admin.site.register(Subscription)
-admin.site.register(Setting)
+
+@admin.register(Contact)
+class ManagmentAdmin(TranslationAdmin):
+    list_display = ('name', 'subject', 'text')
+
+@admin.register(Blog)
+class ManagmentAdmin(TranslationAdmin):
+    list_display = ('title', 'description')
+
+@admin.register(Setting)
+class ManagmentAdmin(TranslationAdmin):
+    list_display = ('name', 'title', 'about_us')
+
